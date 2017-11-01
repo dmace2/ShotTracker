e1385494e2bf92de98fb4a31ccff7714a41aaab9
Dylan Mace <dtmace2@gmail.com>
2017-11-01 10:14:14 -0400

    Initial Commit

---
+++ Shot Tracker! WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Shot Tracker! WatchKit Extension
+//
+//  Created by Dylan Mace on 11/1/17.
+//  Copyright © 2017 Dylan Mace. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
