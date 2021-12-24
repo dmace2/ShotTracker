9a601aa013d57fe38402bc053c641ed7fea9c423
Dylan Mace <dtmace2@gmail.com>
2021-12-24 16:14:42 -0500

    remove creds

--- Pods/Target Support Files/Pods-Shot Tracker! WatchKit Extension/Pods-Shot Tracker! WatchKit Extension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shot_Tracker__WatchKit_Extension : NSObject
-@end
-@implementation PodsDummy_Pods_Shot_Tracker__WatchKit_Extension
-@end
--- Pods/Target Support Files/Pods-Shot Tracker!/Pods-Shot Tracker!-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shot_Tracker_ : NSObject
-@end
-@implementation PodsDummy_Pods_Shot_Tracker_
-@end
